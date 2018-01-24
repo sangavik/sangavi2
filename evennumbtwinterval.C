9e9b4ec1a34f9339c59cfbc63840420bd3765091
sangavik <31438501+sangavik@users.noreply.github.com>
2018-01-24 16:25:39 +0530

    evennumbtwinterval.C

---
+++ evennumbtwinterval.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int f1,f2,re,i;
+printf("enter the first num range:%d");
+scanf("%d",&f1);
+printf("enter the second num range:%d");
+scanf("%d",&f2);
+printf("the even number between %d and %d are:"f1,f2);
+for(i=f1;i<=f2;++i)
+{
+re=i%2;
+if(re%2==0)
+{
+printf("\n%d,i);
+}
+return 0;
+}
