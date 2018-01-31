7c66c3b230555dd8c8fddc36eaecbf9086bd833b
sangavik <31438501+sangavik@users.noreply.github.com>
2018-01-31 15:55:39 +0530

    5mul.C

---
+++ 5mul.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,num,result;
+printf("\n enter the number");
+scanf("%d",&num);
+for(i=0;i<=5;i++)
+{
+result=result*i;
+}
+printf("\n ressult is");
+return 0;
+}
+
