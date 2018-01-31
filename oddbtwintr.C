2010e70850be2843ba8f9f7a047e8e900cb3f3fd
sangavik <31438501+sangavik@users.noreply.github.com>
2018-01-31 16:18:11 +0530

    oddbtwintr.C

---
+++ oddbtwintr.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,range,i=0;
+printf("\n enter the range of num:");
+scanf("%d",&range);
+for(i=n1;i<n2;i++)
+{
+if(i%2==1)
+printf("\n i");
+}
+return 0;
+}
